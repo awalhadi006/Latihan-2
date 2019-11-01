dff0cd776d51676d19346900159ad78e9de40a94
awalhadi006 <awalhadi006@gmail.com>
2019-11-01 16:19:34 +0700

    Lab 2

---
+++ Lab 2.py
@@ -0,0 +1,11 @@
+a=int(input("Masukkan Nilai a:"))
+b=int(input("Masukkan Nilai b:"))
+print("Variable a=",a)
+print("Variable b=",b)
+print("hasil penggabungan {1}&{0}=%d".format(a,b) %(a+b))
+
+#Konversi Nilai Variable
+a=int(a)
+b=int(b)
+print("hasil penjumlahan {1}+{0}=%d".format(a,b) %(a+b))
+print("hasil pembagian {1}/{0}=%d".format(a,b) %(a/b))
